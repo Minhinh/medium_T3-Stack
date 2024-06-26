199381c831adce7b9610f3abaf81f2506114a9e6
Minhinh <nhatminhtran34@gmail.com>
2024-06-27 02:47:14 +1000

    Story

---
+++ src/app/story/page.tsx
@@ -0,0 +1,8 @@
+export default function Page(){
+  return (
+    <div>
+      <h1>Our Story</h1>
+      <p>This is the story of our company, how we started, our journey, and where we are now.</p>
+    </div>
+  );
+};
