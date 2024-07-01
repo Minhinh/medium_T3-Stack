4d2406372af044c14be69d85aa87c15a6f21c7f4
Minhinh <nhatminhtran34@gmail.com>
2024-07-02 01:10:30 +1000

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,19 @@
+'use client'
 import { Navbar } from "../_components/navbar";
 import { Footer } from "../_components/footer";
-import { api } from "~/trpc/server";
-import { useRouter } from "next/router";
+import { api } from "~/trpc/react";
 
 const HomePage = () => {
-  const name = "Bon";
-  const result = api.post.hello(name);
+  const name = 'Bon';
+  const { data, isLoading, error } = api.post.hello.useQuery({
+    text: name,
+  });
 
   return (
     <div>
       <Navbar />
       <div className="flex min-h-screen flex-col items-center justify-center bg-[#f7f4ed]">
-        <h1>{result}</h1>
+        <h1>{data?.greeting}</h1>
         <h1 className="text-2xl">Welcome to the homepage!</h1>
       </div>
       <Footer />
