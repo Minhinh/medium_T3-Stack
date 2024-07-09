f49e69d73c358a3d9107a69e50d3f95460dca954
Minhinh <nhatminhtran34@gmail.com>
2024-07-09 22:21:35 +0700

    type post-preview

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -1,12 +1,27 @@
 'use client';
 
-export const PostPreview = ({ post }) => {
+import { FC } from "react";
+
+interface Post {
+  id: number;
+  title: string;
+  name: string;
+  imageUrl?: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface PostPreviewProps {
+  post: Post;
+}
+
+export const PostPreview: FC<PostPreviewProps> = ({ post }) => {
   return (
     <div className="p-4 mb-6 border rounded bg-white shadow-md">
       <h2 className="text-2xl font-bold mb-2">{post.title}</h2>
       <p className="text-gray-600 mb-4">{post.name}</p>
       {post.imageUrl && (
-        <img src={post.imageUrl} alt={post.title} className="w-full h-auto mb-4"/>
+        <img src={post.imageUrl} alt={post.title} className="w-full h-auto mb-4" />
       )}
       <p className="text-gray-400 text-sm">
         Created at: {new Date(post.createdAt).toLocaleString()}
